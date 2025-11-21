4946389dde426c9e36613986bf5aa83179919706
Anne201 <118590269+Anne201@users.noreply.github.com>
2025-11-21 10:19:12 +0800

    V1

---
+++ multi10/basketball_menu_set.js
@@ -0,0 +1,9 @@
+menu_title = "10以內的加法";
+menu_items = new Array(
+  "1+ 心算,data/add-1.js"
+, "2+ 心算,data/add-2.js"
+, "3+ 心算,data/add-3.js"
+, "4+/5+ 心算,data/add-4.js"
+, "6+/7+ 心算,data/add-5.js"
+, "8+/9+/0+ 心算,data/add-6.js"
+);
